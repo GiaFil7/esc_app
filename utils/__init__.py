174643af34c60d16e09764d8fdda4073772955a5
GiaFil7 <GiaFil007@gmail.com>
2024-12-01 14:45:59 +0200

    Added util function to save a widget as an image

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,11 @@
-from utils.utils_ui import load_widget
+from utils.utils_ui import load_widget, save_widget_to_file
 from utils.utils_data import get_contest_data, update_contest_data
 from utils.utils_data import get_entry_data, update_entry_data
 from utils.utils_data import get_contest_name, get_countries, get_country_codes
 
 __all__ = [
     'load_widget',
+    'save_widget_to_file',
     'get_contest_data',
     'update_contest_data',
     'get_entry_data',
--- utils/utils_ui.py
+++ utils/utils_ui.py
@@ -1,3 +1,5 @@
+from PySide6.QtWidgets import QFileDialog
+
 def load_widget(current_widget: object, widget_to_load: object):
     """
     Loads a widget by setting it as the current widget of the
@@ -12,3 +14,19 @@ def load_widget(current_widget: object, widget_to_load: object):
     current_widget.stacked_widget = current_widget.parent()
     current_widget.stacked_widget.addWidget(widget_to_load)
     current_widget.stacked_widget.setCurrentWidget(widget_to_load)
+
+def save_widget_to_file(parent_widget: object, widget_to_save: object):
+    """
+    Saves a widget as an image at the location and with the name that the
+    user chose using the save file prompt.
+
+    :param parent_widget: The widget the save file prompt originates from
+    :type parent_widget: object
+    :param widget_to_save: The widget to save as an image
+    :type widtget_to_save: object
+    """
+
+    filename, _ = QFileDialog.getSaveFileName(parent_widget, ("Save File"), "./", ("Images (*.png *.jpg)"))
+    
+    img = widget_to_save.grab(widget_to_save.rect())
+    img.save(filename)
