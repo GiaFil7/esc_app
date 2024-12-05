6cf1fcaa6f5932eec7028bf2a42c9520f48acb81
GiaFil7 <GiaFil007@gmail.com>
2024-12-05 20:37:30 +0200

    Added the quiz table

--- quizzes/quizzes_widget.py
+++ quizzes/quizzes_widget.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QWidget
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtGui import QPixmap, Qt
 from ui.ui_quizzes_widget import Ui_quizzes_widget
 from utils import load_widget, get_country_code
 from functools import partial
@@ -13,12 +13,13 @@ class quizzes_widget(QWidget, Ui_quizzes_widget):
         # Initialise properties
         self.quiz_name = quiz_name
         self.quiz_type = quiz_type
-        self.contest_code = parent_menu.contest_code
-        self.contest_name = parent_menu.contest_name
+        self.parent_menu = parent_menu
+        self.contest_code = self.parent_menu.contest_code
+        self.contest_name = self.parent_menu.contest_name
         self.is_paused = True
 
         # Setup slots
-        self.back_button.clicked.connect(partial(load_widget, self, parent_menu))
+        self.back_button.clicked.connect(partial(load_widget, self, self.parent_menu))
         self.play_pause_button.clicked.connect(self.toggle_quiz_state)
         self.give_up_button.clicked.connect(self.end_quiz)
 
@@ -34,8 +35,8 @@ class quizzes_widget(QWidget, Ui_quizzes_widget):
 
             case "year":
                 split_text = self.quiz_name.split(" ")
-                year = split_text[-1]
-                logo_path = f":/images/contest_logos/{self.contest_code}/{self.contest_code}_{year}.png"
+                self.year = split_text[-1]
+                logo_path = f":/images/contest_logos/{self.contest_code}/{self.contest_code}_{self.year}.png"
 
             case _:
                 logo_path = ":/images/heart_logos/empty_heart.svg"
@@ -44,6 +45,8 @@ class quizzes_widget(QWidget, Ui_quizzes_widget):
 
         # Keep give up button hidden until the quiz starts
         self.give_up_button.hide()
+
+        self.setup_table()
 
 
     def toggle_quiz_state(self):
@@ -56,3 +59,72 @@ class quizzes_widget(QWidget, Ui_quizzes_widget):
 
     def end_quiz(self):
         print("Quiz ended")
+
+    def setup_table(self):
+        cols, table_data, accepted_answers = self.get_table_data()
+
+        self.handle_accepted_answers(accepted_answers)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(len(cols))
+        self.table.setHorizontalHeaderLabels(cols)
+        self.table.setRowCount(len(table_data[0]))
+        self.table.verticalHeader().setVisible(False)
+
+        for i in range(len(table_data[0])):
+            for j in range(len(cols)):
+                inds = [i, j]
+                text = table_data[j][i]
+                self.set_table_item(inds, text)
+
+        self.scroll_area.setWidget(self.table)
+
+        # Resize the columns to fit the contents
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.ResizeToContents)
+    
+    def get_table_data(self):
+        entry_data = self.parent_menu.entry_data
+
+        match self.quiz_type:
+            case "country":
+                cols = ["Year", "Song", "Placing"]
+                entries = entry_data[entry_data['country'] == self.quiz_name]
+                
+                years = entries['contest'].to_string(index = False)
+                years = years.split("\n")
+                years = [item.split(" ") for item in years]
+                years = [item[1] for item in years]
+
+                songs = list(entries['song'])
+                placings = entries['placing'].astype(str).values.tolist()
+
+                table_data = [years, songs, placings]
+                
+            case "year":
+                cols = ["Country", "Song", "Placing"]
+                contest = f"{self.contest_code} {self.year}"
+                entries = entry_data[entry_data['contest'] == contest]
+
+                countries = list(entries['country'])
+                songs = list(entries['song'])
+                placings = entries['placing'].astype(str).values.tolist()
+
+                table_data = [countries, songs, placings]
+
+            case _:
+                cols = ["Thing", "Song", "Hint"] # Temporary
+
+        accepted_answers = list(entries['accepted_answers'])
+
+        return cols, table_data, accepted_answers
+
+    def handle_accepted_answers(self, accepted_answers: list):
+        #print(accepted_answers)
+        x = []
+
+    def set_table_item(self, inds: list, text: str):
+        widget_item = QTableWidgetItem(text)
+        widget_item.setTextAlignment(Qt.AlignCenter)
+        widget_item.setFlags(Qt.ItemIsEnabled)
+        self.table.setItem(inds[0], inds[1], widget_item)
