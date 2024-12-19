686d266cfa04563f6242b52f79e0bbd2d3f1650b
GiaFil7 <GiaFil007@gmail.com>
2024-12-19 21:15:45 +0200

    Fonts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QFontDatabase
 from main_window import main_window
 import sys
 
@@ -8,7 +9,15 @@ def main():
     window = main_window()
     window.show()
 
-    with open("styles.qss", "r") as f:
+    QFontDatabase.addApplicationFont('styles\\Roboto.ttf')
+    QFontDatabase.addApplicationFont('styles\\AlfaSlabOne.ttf')
+    QFontDatabase.addApplicationFont('styles\\Lexend.ttf')
+    QFontDatabase.addApplicationFont('styles\\Shrikhand.ttf')
+    QFontDatabase.addApplicationFont('styles\\Teko.ttf')
+    QFontDatabase.addApplicationFont('styles\\TitanOne.ttf')
+    QFontDatabase.addApplicationFont('styles\\Ultra.ttf')
+
+    with open("styles\\styles.qss", "r") as f:
         _style = f.read()
         app.setStyleSheet(_style)
 
